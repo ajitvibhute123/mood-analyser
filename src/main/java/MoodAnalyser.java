90261c82593fcba1993ae9180fe7c6e77dcacccd
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-04-03 16:24:13 +0530

    Adding welcome message

---
+++ src/main/java/MoodAnalyser.java
@@ -0,0 +1,14 @@
+
+/**
+ * Purpose -to analyse and respond happy or sad mood
+ * 
+ * @author Ajit Vibhute
+ * @since - 02-04-2022
+ *
+ */
+public class MoodAnalyser {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the mood analyser program");
+	}
+}
